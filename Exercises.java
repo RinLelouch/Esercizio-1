056867a64ced89da93084adec56d2a88b73e1d73
Andrea Occhiello <126583065+RinLelouch@users.noreply.github.com>
2023-04-03 00:02:54 +0200

    Add files via upload
    ex 2 advanced

--- Exercises.java
+++ Exercises.java
@@ -1,145 +1,109 @@
-package AdvancedEx1;
+package AdvancedEx2;
 
-import java.util.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Exercises {
-
     public static void main(String[] args) {
         exercise1();
         exercise2();
         exercise3();
-        exercise4();
     }
 
     /**
-     * 1:
-     * <p>
-     * 1. Add some numbers (1, 1, 5, 2, 1) to the set using the add() method.
-     * 2. Remove '1' from  the set using the .remove(1)
-     * 3. Print the set again to confirm the element was removed.
+     * 1: Follow the steps below
      */
     private static void exercise1() {
-        System.out.println("Exercise 1: ");
-        Set<Integer> ourSet = new HashSet<Integer>();
-        ourSet.add(1);
-        ourSet.add(1);
-        ourSet.add(5);
-        ourSet.add(2);
-        ourSet.add(1);
-
-        System.out.println(ourSet);
-
-        // Add some numbers using the add method
-        // Your code
-
-        if (ourSet.containsAll(Set.of(1, 5, 2))) {
-            System.out.println("1a. Set contains all numbers");
-        } else {
-            System.out.println("1a. Set does not contain all numbers");
+        // 1a. Create a map of integers to strings
+        Map<Integer, String> myFirstMap = new HashMap<>();
+        myFirstMap.put(25, "Andrea");
+        myFirstMap.put(20, "Paolo");
+        myFirstMap.put(22, "Angela");
+        for (Integer key: myFirstMap.keySet()) {
+            System.out.println(key + " " + myFirstMap.get(key));
+        }
+        System.out.println("\n");
+        myFirstMap.get(20);
+        myFirstMap.remove(25);
+        for (Integer key: myFirstMap.keySet()) {
+            System.out.println(key + " " + myFirstMap.get(key));
         }
 
-        System.out.println(ourSet);
-        ourSet.remove(1);
-        // Remove the element '1'
-        // Your code
 
-        System.out.println(ourSet);
-
-        if (ourSet.contains(1)) {
-            System.out.println("1b. The set still contains 1");
-        } else {
-            System.out.println("1b. The set does not contain 1");
-        }
+        // 1b. Add 3 elements to the map .put
+        // 1c. Print the map
+        // 1d. Access an element in the map with .get
+        // 1e. Remove an element from the map .remove
+        // 1f. Print the map again to confirm the element was removed
     }
 
+
     /**
-     * 2:
-     * <p>
-     * Read the comments below and write comments on what you think our print statement will output!
+     *  2: Follow the instructions in the code
      */
     private static void exercise2() {
-        Set<String> fruitsSet = new HashSet<>();
+        Map<String, Double> menuCostsInEuro = new HashMap<>();
 
-        fruitsSet.add("apple");
-        fruitsSet.add("banana");
-        fruitsSet.add("orange");
-        fruitsSet.add("kiwi");
-        fruitsSet.add("mango");
+        menuCostsInEuro.put("Fries", 3.50);
+        menuCostsInEuro.put("Pizza", 11.80);
+        menuCostsInEuro.put("Burger", 8.50);
 
-        fruitsSet.addAll(fruitsSet);
+        String mostExpensiveMenuItemName = null;
+        double valoreDiPartenza = Double.MIN_VALUE;
 
-        //2a. What will this output? [banana,apple,orange,kiwi,mango]
-        System.out.println(fruitsSet);
+        System.out.println(menuCostsInEuro);
+        // 2a. Loop over the map entries with menuCostsInEuro.entrySet()
+        //     and set out the most expensive items name
+        //     use for(Map.Entry<String, Double> entry : menuCostsInEuro.entrySet()) and entry.getKey and entry.getValue
+        //     to do so
+        for (Map.Entry<String, Double> n : menuCostsInEuro.entrySet()){
+            if(n.getValue() > valoreDiPartenza){
+                mostExpensiveMenuItemName = n.getKey();
+                valoreDiPartenza = n.getValue();
+            }
+        }
 
-        List<String> fruitsList = List.of("apple", "banana", "grapes", "pear", "mango");
+        if ("Pizza".equals(mostExpensiveMenuItemName)) {
+            System.out.println("2a. Correct!");
+        } else {
+            System.out.println("2a. Incorrect!");
+        }
 
-        fruitsSet.addAll(fruitsList);
+        // Your code
 
-        //2b. What will this output? [banana, orange, apple, kiwi, pear, mango, grapes]
-        System.out.println(fruitsSet);
-    }
+
+   }
+
 
     /**
-     * 3: Write code to solve the following problems
+     * 3: Follow the instructions in the code
      */
     private static void exercise3() {
-        // 3a. Find the min and max values in the Set below
-        Set<Integer> numbers = Set.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        for (Integer number : numbers) {
-            if (number>max){
-                max=number;
-            } if(number<min){
-                min=number;
-            }
-        }
-        System.out.println(max);
-        System.out.println(min);
+        Map<String, Integer> fruitWeights = new HashMap<>();
 
-        if (max != 10 || min != 1) {
-            System.out.println("3a. Incorrect min or max values");
-        }
+        fruitWeights.put("Apple", 5);
+        fruitWeights.put("Banana", 3);
+        fruitWeights.put("Orange", 8);
+        fruitWeights.put("Kiwi", 1);
+        fruitWeights.put("Mango", 4);
+        fruitWeights.put("Grapes", 8);
+        fruitWeights.put("Peach", 5);
+        fruitWeights.put("Pineapple", 1);
 
-        // 3b. Find which items from set a are also in set b, add these to itemsContainedInBothSets
-        //     use the `contains` method and "nested" for loop
-        Set<Integer> setA = Set.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        Set<Integer> setB = Set.of(5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
-        Set<Integer> itemsContainedInBothSets = new HashSet<>();
-        for (int n : setA){
-            if (setB.contains(n)){
-                itemsContainedInBothSets.add(n);
-            }
+
+        int totalWeight = 0;
+        // 3a. Use a for loop with fruitWeights.values() and sum up the total weight
+       for (Integer weight : fruitWeights.values() ){
+           totalWeight += weight;
         }
 
         // Your code
 
-        if (!itemsContainedInBothSets.equals(Set.of(5, 6, 7, 8, 9, 10))) {
-            System.out.println("3b. Incorrect number of items in itemsContainedInBothSets");
+        if (totalWeight == fruitWeights.values().stream().mapToInt(Integer::intValue).sum()) {
+            System.out.println("3. Correct!");
         } else {
-            System.out.println("3b. Correct number of items in itemsContainedInBothSets");
-        }
-    }
-
-    /**
-     * 4: BONUS! This is an optional task, but it is recommended you complete it
-     * ----------------------------------------------------------------------
-     */
-    private static void exercise4() {
-        Set<Integer> orderedNumbers = new TreeSet<>();
-        orderedNumbers.addAll(Set.of(5, 3, 1, 2, 4, 6, 7, 8, 9, 10));
-        // 4a. Find the min value in the TreeSet with as few loops as possible,
-        //     you can use `break;` to exit the loop once you've found it!
-        int min = Integer.MAX_VALUE;
-        for (int num : orderedNumbers) {
-            min = Math.min(num, min);
-        }
-        // Your code
-
-        if (min != 1) {
-            System.out.println("4a. Incorrect min value");
-        } else {
-            System.out.println("4a. Correct min value");
+            System.out.println("3. Incorrect!");
         }
     }
 }
