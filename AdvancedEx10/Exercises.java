7234620196ff58388f1b47b159ccccb877f4edcf
Andrea Occhiello <126583065+RinLelouch@users.noreply.github.com>
2023-04-18 13:36:28 +0200

    Add files via upload

---
+++ AdvancedEx10/Exercises.java
@@ -0,0 +1,57 @@
+package AdvancedEx10;
+
+public class Exercises {
+
+    public static void main(String[] args) {
+        exercise1();
+        exercise2();
+    }
+    /**
+     * 1:
+     *
+     *
+     * Create a class called "Person" with fields for name, age, and address.
+     *
+     * Override the `toString()` method in the Person class to display the object's name, age, and address in a user-friendly format.
+     *
+     * Create instances of the Person class and print them using the overridden `toString()` method.
+     */
+    private static void exercise1() {
+        System.out.println("\nExercise 1: ");
+        // Your code here
+        Person person = new Person("Andrea", 25, "Via Cavalli di Bronzo" );
+        System.out.println(person);
+    }
+
+    /**
+     * 2:
+     *
+     *
+     * Override the `hashCode()` method in the Person class to generate a unique hash code based on the object's name, age, and address.
+     *
+     * You can achieve this by using String.hashCode() to convert strings to ints and adding the fields together
+     *
+     * Create 2 people and print out their hashcodes
+     */
+    private static void exercise2() {
+        System.out.println("\nExercise 2: ");
+        // Your code here
+        Person person = new Person("Andrea", 25, "Via Cavalli di Bronzo" );
+        Person person1 = new Person("Giovanni", 25, "Via Asini di Bronzo" );
+        Person person2 = new Person("Andrea", 23, "Via giuseppe di Bronzo" );
+        System.out.println(person.hashCode());
+        System.out.println(person1.hashCode());
+        System.out.println(person2.hashCode());
+    }
+
+    /**
+     * 3:
+     *
+     * BONUS: Read about the nuances of hashcodes here:
+     *
+     * https://www.baeldung.com/java-hashcode
+     *
+     * Don't necessarily expect to understand it all right away, it can get quite complicated
+     *
+     */
+}
