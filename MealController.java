2b9538105e15b25d3b7f19cc610467781f116c74
Andrea Occhiello <126583065+RinLelouch@users.noreply.github.com>
2023-07-05 19:16:00 +0200

    Add files via upload

---
+++ MealController.java
@@ -0,0 +1,38 @@
+package Esercizio_2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class MealController {
+    @Autowired
+    private MealService mealService;
+    @PutMapping (value = "/insert-meal")
+    public ResponseEntity<Meal> addMeal(@RequestBody Meal meal){
+        mealService.addMeal(meal);
+        return ResponseEntity.ok(meal);
+
+    }
+    @GetMapping(value ="/meals")
+    public ResponseEntity<List<Meal>> getMeals(){
+        return ResponseEntity.ok(mealService.getMeals());
+    }
+    @GetMapping (value = "/meal/{name}")
+    public ResponseEntity<List<Meal>> getMealByName (@PathVariable("name") String name){
+
+        return ResponseEntity.ok( mealService.getMeals().stream().filter(meal -> meal.getName().equals(name)).toList());
+    }
+    @GetMapping (value = "/meal/description-match/{phrase}")
+    public ResponseEntity<List<Meal>> getMealByDescription(@PathVariable ("phrase") String phrase){
+        return ResponseEntity.ok( mealService.getMeals().stream().filter(meal -> meal.getName().contains(phrase)).toList());
+    }
+    @GetMapping (value = "/meal/{priceMax}-{priceMin}")
+    public ResponseEntity<?> getMealByPrice(@RequestParam ("priceMax") Integer priceMax ,
+    @RequestParam ("priceMin") Integer priceMin){
+            return ResponseEntity.ok(mealService.getMeals().stream().filter(meal -> meal.getPrice()> priceMin && meal.getPrice() < priceMax ).toList())
+    }
+}
